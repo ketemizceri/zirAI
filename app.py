029c5e05781e44dce1a9620b663ca5adc17bce65
Hkn <hakantemirkaynak2@gmail.com>
2024-12-26 22:45:29 +0300

    app.py
    Hasat bölümü flask kodları ve gerekli html dosyaları

--- app.py
+++ app.py
@@ -240,3 +240,107 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    from flask import Flask, render_template, request
+
+    app = Flask(__name__)
+
+
+    class ToprakVerimlilik:
+        def __init__(self, om, npk, max_om, max_npk):
+            self.__om = om
+            self.__npk = npk
+            self.__max_om = max_om
+            self.__max_npk = max_npk
+
+        def hesapla_tfe(self):
+            if (self.__max_om == 0) or (self.__max_npk == 0):
+                raise ValueError("Maksimum Organik Madde Orani veya Maksimum Besin Miktari 0 Olamaz!")
+            return (self.__om * self.__npk) / (self.__max_om * self.__max_npk)
+
+
+    class SuKullanimiEndeksi:
+        def __init__(self, mevcut_su, bitki_su_ihtiyaci):
+            self.__mevcut_su = mevcut_su
+            self.__bitki_su_ihtiyaci = bitki_su_ihtiyaci
+
+        def hesapla_sye(self):
+            if self.__bitki_su_ihtiyaci == 0:
+                raise ValueError("Bitki Su İhtiyaci 0 Olamaz!")
+            sye = self.__mevcut_su / self.__bitki_su_ihtiyaci
+            return min(sye, 1)
+
+
+    class IklimUygunlukKatsayisi:
+        def __init__(self, mevcut_sicaklik, optimum_sicaklik):
+            self.__mevcut_sicaklik = mevcut_sicaklik
+            self.__optimum_sicaklik = optimum_sicaklik
+
+        def hesapla_iak(self):
+            if self.__optimum_sicaklik == 0:
+                raise ValueError("Optimum Sicaklik 0 Olamaz!")
+            iak = self.__mevcut_sicaklik / self.__optimum_sicaklik
+            return min(iak, 1)
+
+
+    class TarlaYonetimFaktoru:
+        def __init__(self, gf, sf, zf):
+            self.__gf = gf
+            self.__sf = sf
+            self.__zf = zf
+
+        def hesapla_tmf(self):
+            if not (0 <= self.__gf <= 1 and 0 <= self.__sf <= 1 and 0 <= self.__zf <= 1):
+                raise ValueError("Katsayilar 0 ile 1 arasinda olmalidir.")
+            tmf = self.__gf * self.__sf * self.__zf
+            return tmf
+
+
+    class HasatMiktari:
+        def __init__(self, um, tfe, sye, iak, tmf):
+            self.__um = um
+            self.__tfe = tfe
+            self.__sye = sye
+            self.__iak = iak
+            self.__tmf = tmf
+
+        def hesapla_h(self):
+            tfe = self.__tfe.hesapla_tfe()
+            sye = self.__sye.hesapla_sye()
+            iak = self.__iak.hesapla_iak()
+            tmf = self.__tmf.hesapla_tmf()
+            h = self.__um * tfe * sye * iak * tmf
+            return h
+
+
+    @app.route('/', methods=['GET', 'POST'])
+    def index():
+        if request.method == 'POST':
+            um = float(request.form['um'])
+            om = float(request.form['om'])
+            npk = float(request.form['npk'])
+            max_om = float(request.form['max_om'])
+            max_npk = float(request.form['max_npk'])
+            mevcut_su = float(request.form['mevcut_su'])
+            bitki_su_ihtiyaci = float(request.form['bitki_su_ihtiyaci'])
+            mevcut_sicaklik = float(request.form['mevcut_sicaklik'])
+            optimum_sicaklik = float(request.form['optimum_sicaklik'])
+            gf = float(request.form['gf'])
+            sf = float(request.form['sf'])
+            zf = float(request.form['zf'])
+
+            tfe_obj = ToprakVerimlilik(om, npk, max_om, max_npk)
+            sye_obj = SuKullanimiEndeksi(mevcut_su, bitki_su_ihtiyaci)
+            iak_obj = IklimUygunlukKatsayisi(mevcut_sicaklik, optimum_sicaklik)
+            tmf_obj = TarlaYonetimFaktoru(gf, sf, zf)
+
+            hasat = HasatMiktari(um, tfe_obj, sye_obj, iak_obj, tmf_obj)
+            h = hasat.hesapla_h()
+
+            return render_template('result.html', h=h, tfe=tfe_obj.hesapla_tfe(), sye=sye_obj.hesapla_sye(),
+                                   iak=iak_obj.hesapla_iak(), tmf=tmf_obj.hesapla_tmf())
+
+        return render_template('index.html')
+
+
+    if __name__ == '__main__':
+        app.run(debug=True)
